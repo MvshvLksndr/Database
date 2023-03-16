2aaabb9c2895f93175e40e4ffed412cb3b138735
stud <stud@107-5>
2023-03-16 12:51:59 +0400

    подключенее к базе

---
+++ DB/MS_SQL_Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDB.DB
+{
+    internal class MS_SQL_Context : DbContext
+    {
+        string connectionString = 
+        "Server=192.168.49.180; Database=LksndrDB_test; User Id = stud; Password=stud; TrustServerCertificate=true;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
